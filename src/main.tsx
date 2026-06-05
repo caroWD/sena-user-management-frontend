b334c7f526d9e462fed9a4fdc949a1997403e317
caroWD <juego.fuego@gmail.com>
2026-06-05 16:12:06 -0500

    feat: add and configure react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import './index.css'
-import App from './App.tsx'
 import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { RouterProvider } from 'react-router'
+import { routes } from './routes.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={routes} />
     </ThemeProvider>
   </StrictMode>
 )
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router'
+import App from './pages/App'
+
+export const routes = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+  },
+])
